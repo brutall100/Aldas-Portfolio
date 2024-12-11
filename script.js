356fe890dd133e766d17f230b26aa8be4c946d59
brutall100 <37956008+brutall100@users.noreply.github.com>
2024-12-12 00:30:15 +0200

    Js to year element

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const currentYearElement = document.getElementById('current-year')
+	const currentYear = new Date().getFullYear()
+	currentYearElement.textContent = currentYear
+})
